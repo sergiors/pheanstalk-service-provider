68b0a8e490ae9a493a4880f7ecb96c71a9e5ac9b
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2015-12-11 12:34:17 -0200

    Add all commands

--- src/Provider/PheanstalkServiceProvider.php
+++ src/Provider/PheanstalkServiceProvider.php
@@ -8,8 +8,19 @@ use Pheanstalk\Pheanstalk;
 use Pheanstalk\PheanstalkInterface;
 use Leezy\PheanstalkBundle\PheanstalkLocator;
 use Leezy\PheanstalkBundle\Listener\PheanstalkLogListener;
+use Leezy\PheanstalkBundle\Command\DeleteJobCommand;
+use Leezy\PheanstalkBundle\Command\FlushTubeCommand;
+use Leezy\PheanstalkBundle\Command\KickCommand;
+use Leezy\PheanstalkBundle\Command\KickJobCommand;
 use Leezy\PheanstalkBundle\Command\ListTubeCommand;
+use Leezy\PheanstalkBundle\Command\NextReadyCommand;
+use Leezy\PheanstalkBundle\Command\PauseTubeCommand;
+use Leezy\PheanstalkBundle\Command\PeekCommand;
+use Leezy\PheanstalkBundle\Command\PeekTubeCommand;
+use Leezy\PheanstalkBundle\Command\PutCommand;
 use Leezy\PheanstalkBundle\Command\StatsCommand;
+use Leezy\PheanstalkBundle\Command\StatsJobCommand;
+use Leezy\PheanstalkBundle\Command\StatsTubeCommand;
 
 /**
  * @author Sérgio Rafael Siqueira <sergio@gmail.com>
@@ -84,8 +95,19 @@ class PheanstalkServiceProvider implements ServiceProviderInterface
                 $app->extend('console', function (ConsoleApplication $console) use ($app) {
                     $locator = $app['pheanstalks'];
 
+                    $console->add(new DeleteJobCommand($locator));
+                    $console->add(new FlushTubeCommand($locator));
+                    $console->add(new KickCommand($locator));
+                    $console->add(new KickJobCommand($locator));
                     $console->add(new ListTubeCommand($locator));
+                    $console->add(new NextReadyCommand($locator));
+                    $console->add(new PauseTubeCommand($locator));
+                    $console->add(new PeekCommand($locator));
+                    $console->add(new PeekTubeCommand($locator));
+                    $console->add(new PutCommand($locator));
                     $console->add(new StatsCommand($locator));
+                    $console->add(new StatsJobCommand($locator));
+                    $console->add(new StatsTubeCommand($locator));
 
                     return $console;
                 })
